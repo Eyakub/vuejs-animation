ed41094623ec294649a07c20738b676e167d5a47
eyakub <eyakubsorkar@gmail.com>
2019-08-17 02:38:19 +0600

    vuejs custom animation css implementing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
     <div style="margin-top: 200px;">
         <button class="btn btn-primary" @click="show = !show">Toggle</button>
-        <transition 
-            enter-active-class="animated pulse slow"
-
-            leave-active-class="animated zoomOut slow">
+        <transition>
             <p v-if="show" style="width: 200px;">Hi there its me, Animation</p>
         </transition>
     </div>
@@ -23,7 +20,35 @@ export default {
 </script>
 
 <style>
-    .slow{
-        animation-duration: 3s;
+    .v-enter-active{
+        animation: pulse 1s;
+    }
+
+    .v-leave-active{
+        animation: fade-out 1s;
+    }
+
+    @keyframes fade-out {
+        /* initial state */
+        from{
+            opacity: 1;
+            transition: opacity;
+        }
+        /* end state */
+        to {
+            opacity: 0;
+        }
+    }
+
+    @keyframes pulse{
+        from {
+            transform: scale3d(1, 1, 1);
+        }
+        50%{
+            transform: scale3d(1.2, 1.2, 1.2);
+        }
+        to{
+            transform: scale3d(1, 1, 1);
+        }
     }
 </style>
