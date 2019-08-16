ddea679286b365d20f0704582fca9096b5dff49e
eyakub <eyakubsorkar@gmail.com>
2019-08-16 23:54:18 +0600

    vuejs fadein-out using vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div style="margin-top: 200px;">
+        <button class="btn btn-primary" @click="show = !show">Toggle</button>
+        <transition>
+            <p v-if="show">Hi there its me, Animation</p>
+        </transition>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return {
+            show: false
+        };
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-enter, .v-leave-to{
+    opacity: 0;
+}
+
+.v-enter-active, .v-leave-active{
+    transition: opacity 1s;
 }
 </style>
